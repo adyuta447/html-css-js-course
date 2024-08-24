0a22d18e9bdc6516375223ca76530547a7c3276b
K0oceng <yutaagra@gmail.com>
2024-08-24 19:11:09 +0700

    Type: Updated Pengenalan HTML

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  logo: <span>K0oceng</span>,
+  project: {
+    link: "https://github.com/adyuta447/learn-html-css",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+  docsRepositoryBase: "https://github.com/adyuta447/learn-html-css",
+  footer: {
+    text: "Build by Adyuta",
+  },
+};
+
+export default config;
